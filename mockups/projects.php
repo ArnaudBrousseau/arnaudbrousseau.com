34b953e158340dc52e55a873760a99131c03eaa8
Arnaud Brousseau <arnaud.brousseau@gmail.com>
2013-12-01 20:37:07 -0800

    Ports old homepage to docpad.

---
+++ mockups/projects.php
@@ -0,0 +1,353 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+    <head>
+
+        <meta name="google-site-verification" content="xaUx_yIq4fjRfe8oxr_1bfvSfz8WVOCYGrJX5PFlyQQ" />
+	<meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0;">
+        <meta name="robots" content="all" />
+        <meta name="description" content="Je suis Arnaud BROUSSEAU. Webdesigner et développeur à mes heures perdues, ceci est mon terrain de jeu en ligne ! Entrez je vous prie... " />
+        <meta name="keywords" content="Arnaud, Brousseau, web, design, software, developpement, html5, css3, javascript, php" />
+        <meta name="author" content="Arnaud BROUSSEAU" />
+
+        <title>Arnaud Brousseau | Playing web.</title>
+
+        <link rel="canonical" href="http://arnaudbrousseau.com/" />
+	<link rel="stylesheet" media="screen" type="text/css" href="/portfolio/css/style.css"/>
+
+	<link rel="shortcut icon" href="/favicon.png">
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png">
+
+        <script src="/portfolio/js/modernizr-1.5.min.js"></script>
+
+
+    </head>
+
+    <body>
+
+    <header>
+
+	<nav>
+
+	    <ul>
+		<a href=""><span>Arnaud Brousseau | Home, sweet home...<span></a>
+		<li><a href=""><span>Home</span></li>
+		<li><a href=""><span>Projets</span></li>
+		<li><a href=""><span>Exp.</span></li>
+		<li><a href=""><span>Contact</span></li>
+	    </ul>
+
+	</nav>
+
+    </header>
+
+
+	<section id="projects">
+
+	    <h1>Missions</h1>
+
+            <nav>
+
+                <ul>
+
+                    <li class="head"><a href="#head" title="Revenir en haut de page"><span>Top</span></a></li>
+
+                    <li class="projects"><a href="#projects" title="Regarder les missions effectuées"><span>Missions</span></a></li>
+
+                    <li class="skills"><a href="#skills" title="Examiner mes compétences"><span>Xp.</span></a></li>
+
+                    <li class="contact"><a href="#contact" title="Me contacter"><span>Contact</span></a></li>
+
+                </ul>
+
+            </nav>
+
+	    <section class="project">
+
+
+
+                <h2>Site du BDE Osmoz</h2>
+
+		<time datetime="2008-10-01">Octobre 2008</time>
+
+
+
+		<div class="projectCaption">
+
+		    <p><img class="w100" src="/portfolio/img/siteOsmoz.jpg" alt="" /></p>
+
+		    <p><img class="w70" src="/portfolio/img/siteOsmozPartenaires.jpg" alt="" /></p>
+
+		    <p><img class="w30" src="/portfolio/img/siteOsmozPresentation.jpg" alt="" /></p>
+
+		</div>
+
+
+
+		<div class="projectDescription">
+
+
+
+		    <p>
+
+			Ce site internet a été pendant plus d'un an le point de contact entre 800 élèves et son BDE.<br/>
+
+			Flux de news, calendrier, jeux, section "pougne", page partenaire...autant de sections mises à profit pour promouvoir et encourager la vie associative extrêmement riche du campus de Télécom SudParis et Télécom Ecole de Management !
+
+		    </p>
+
+
+
+		    <ul>
+
+			<li>Design sous Photoshop</li>
+
+			<li>Mise en forme CSS</li>
+
+			<li>Applications dynamiques avec Php</li>
+
+		    </ul>
+
+		</div>
+
+
+
+		<div class="clear"></div>
+
+
+
+	    </section>
+
+
+
+	    <section class="project">
+
+
+
+                <h2>Page de démarrage Osmoz</h2>
+
+		<time datetime="2008-10-01">Avril 2009</time>
+
+
+
+		<div class="projectCaption">
+
+		    <p><img class="w70" src="/portfolio/img/homepageMenu.jpg" alt="" /></p>
+
+		    <p><img class="w30" src="/portfolio/img/homepageFlux.jpg" alt="" /></p>
+
+		    <p><img class="w100" src="/portfolio/img/homepage.jpg" alt="" /></p>
+
+
+
+		</div>
+
+
+
+		<div class="projectDescription">
+
+		    <p>
+
+			Pendant toute la durée du mandat du BDE Osmoz, cette page a été la page de démarrage de la majorité des élèves du campus de Télécom SudParis et Télécom Ecole de Management.
+
+			Cette page est née d'un double besoin : celui, pour le BDE, d'avoir un point de contact permanent avec les élèves, et celui, pour les élèves, d'avoir un point qui regroupe toutes les infos et les liens utiles lorsqu'ils démarrent leur navigateur.
+
+		    </p>
+
+
+
+		    <ul>
+
+			<li>Design sous Photoshop</li>
+
+			<li>Mise en forme CSS dynamique et fluide</li>
+
+			<li>Page traduite avec système de cookie</li>
+
+		    </ul>
+
+		</div>
+
+
+
+		<div class="clear"></div>
+
+	    </section>
+
+	    <section class="project">
+
+
+
+                <h2>Les gites du Pays d'Honfleur</h2>
+
+		<time datetime="2008-10-01">Mars 2010</time>
+
+
+
+		<div class="projectCaption">
+
+		    <p><img class="w100" src="/portfolio/img/gites.jpg" alt="" /></p>
+
+		    <p><img class="w30" src="/portfolio/img/gitesPhotos.jpg" alt="" /></p>
+
+		    <p><img class="w30" src="/portfolio/img/gitesOnglets.jpg" alt="" /></p>
+
+		    <p><img class="w30" src="/portfolio/img/gitesTarifs.jpg" alt="" /></p>
+
+
+
+		</div>
+
+
+
+		<div class="projectDescription">
+
+		    <p>
+
+			Ce site est la vitrine d'un propriétaire de gites ruraux en Normandie, à Honfleur. Le travail principal a consisté à comprendre les besoins client : gestion du planning, interface administration "user-friendly", design sobre,...
+
+			<br/>Les solutions retenues : un thème Wordpress construit "from scratch" pour s'adapter au mieux aux besoins, et une application PHP pour la partie gestion des réservations et du planning.
+
+		    </p>
+
+
+
+		    <ul>
+
+			<li>Design sous Photoshop</li>
+
+			<li>Gestion du planing à l'aide de Google Agenda, puis construction d'une application PHP</li>
+
+			<li>Gestion du contenu avec Wordpress : développement d'un thème personnalisé</li>
+
+		    </ul>
+
+		</div>
+
+
+
+		<div class="clear"></div>
+
+	    </section>
+
+	    <section class="project">
+
+
+
+                <h2>MUSC France</h2>
+
+                <time datetime="2008-10-01">Juin 2010</time>
+
+
+
+		<div class="projectCaption">
+
+		    <p><img class="w100" src="/portfolio/img/manutd.jpg" alt="" /></p>
+
+		    <p><img class="w60" src="/portfolio/img/manutdSocial.jpg" alt="" /></p>
+
+		    <p><img class="w40" src="/portfolio/img/manutdBilletterie.jpg" alt="" /></p>
+
+		    <p><img class="w40" src="/portfolio/img/manutdPaiement.jpg" alt="" /></p>
+
+
+
+
+
+		</div>
+
+
+
+		<div class="projectDescription">
+
+		    <p>
+
+			MUSC France (Manchester United Supporter Club France) est une association bénévole qui permet aux supporters de Manchester d'aller assister aux matchs de leur équipe préférée.
+
+			<br/>Ce projet web comportait plusieurs modules : fil de news, gestion des adhésions (paiement), forums, billetterie en ligne,...
+
+			<br/>Les solutions que j'ai retenu : Wordpress (encore une fois) pour la rédaction des news, des articles, et la gestion des auteurs, PhpBB pour le forum, et le déploiement d'une application développée par mes soins pour la gestion administrative de la billetterie et des adhésions.
+
+		    </p>
+
+
+
+		    <ul>
+
+			<li>Design sous Photoshop</li>
+
+			<li>Mise en forme CSS, utilisation d'un framework</li>
+
+			<li>Développement et intégration d'un thème Wordpress</li>
+
+			<li>Intégration de réseaux sociaux</li>
+
+			<li>Déploiement d'une billetterie</li>
+
+			<li>Mise en place d'un système de paiement en ligne</li>
+
+		    </ul>
+
+		</div>
+
+
+
+		<div class="clear"></div>
+
+	    </section>
+
+	</section>
+
+
+	<footer>
+
+	    <p>Copyright © 2010 Arnaud Brousseau | Powered by CodeIgniter | Site valide HTML5 | Disposition fluide (redimmensionnez votre navigateur) | Version mobile : à venir</p>
+
+	</footer>
+
+
+
+        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js" type="text/javascript"></script>
+
+	<script>!window.jQuery && document.write('<script src="/portfolio/js/jquery-1.4.2.min.js"><\/script>')</script>
+
+        <script src="/portfolio/js/jquery.scrollTo-1.4.2-min.js" type="text/javascript"></script>
+
+	<script src="/portfolio/js/gaming.js"></script>
+
+        <!--[if lt IE 7 ]>
+
+            <script src="js/dd_belatedpng.js?v=1"></script>
+
+        <![endif]-->
+
+
+
+      <script>
+
+       var _gaq = [['_setAccount', 'UA-XXXXX-X'], ['_trackPageview']];
+
+       (function(d, t) {
+
+        var g = d.createElement(t),
+
+            s = d.getElementsByTagName(t)[0];
+
+        g.async = true;
+
+        g.src = '//www.google-analytics.com/ga.js';
+
+        s.parentNode.insertBefore(g, s);
+
+       })(document, 'script');
+
+      </script>
+
+    </body>
+
+
+
+</html>
---
+++ mockups/skills.php
@@ -0,0 +1,202 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+    <head>
+
+        <meta name="google-site-verification" content="xaUx_yIq4fjRfe8oxr_1bfvSfz8WVOCYGrJX5PFlyQQ" />
+	<meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0;">
+        <meta name="robots" content="all" />
+        <meta name="description" content="Je suis Arnaud BROUSSEAU. Webdesigner et développeur à mes heures perdues, ceci est mon terrain de jeu en ligne ! Entrez je vous prie... " />
+        <meta name="keywords" content="Arnaud, Brousseau, web, design, software, developpement, html5, css3, javascript, php" />
+        <meta name="author" content="Arnaud BROUSSEAU" />
+
+        <title>Arnaud Brousseau | Playing web.</title>
+
+        <link rel="canonical" href="http://arnaudbrousseau.com/" />
+	<link rel="stylesheet" media="screen" type="text/css" href="/portfolio/css/style.css"/>
+
+	<link rel="shortcut icon" href="/favicon.png">
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png">
+
+        <script src="/portfolio/js/modernizr-1.5.min.js"></script>
+
+
+    </head>
+
+    <body>
+
+    <header>
+
+	<nav>
+
+	    <ul>
+		<a href=""><span>Arnaud Brousseau | Home, sweet home...<span></a>
+		<li><a href=""><span>Home</span></li>
+		<li><a href=""><span>Projets</span></li>
+		<li><a href=""><span>Exp.</span></li>
+		<li><a href=""><span>Contact</span></li>
+	    </ul>
+
+	</nav>
+
+    </header>
+
+
+
+	<section id="skills">
+
+	    <h1>Xp.</h1>
+
+
+
+            <nav>
+
+                <ul>
+
+                    <li class="head"><a href="#head" title="Revenir en haut de page"><span>Top</span></a></li>
+
+                    <li class="projects"><a href="#projects" title="Regarder les missions effectuées"><span>Missions</span></a></li>
+
+                    <li class="skills"><a href="#skills" title="Examiner mes compétences"><span>Xp.</span></a></li>
+
+                    <li class="contact"><a href="#contact" title="Me contacter"><span>Contact</span></a></li>
+
+                </ul>
+
+            </nav>
+
+
+
+	    <div class="skill">
+
+		<h2>Maniaco-sémantique</h2>
+
+		<div class="mark9"><p><span>9/10</span></p></div>
+
+		<p class="detail">Je code toutes mes pages web à l'aide de balises (HTML, XHTML ou XML). Jusque là, rien de spécial. Cependant, j'y apport un soin tout particulier : j'essaie de rester aussi sémantique que possible lorsque je code. Cela est déjà un avantage pour les moteurs de recherche actuels, mais la sémantique deviendra sans nul doute un impératif pour l'éco-système de données que sera le web de demain.</p>
+
+	    </div>
+
+	    <div class="skill">
+
+		<h2>Créateur de style</h2>
+
+		<div class="mark8"><p><span>8/10</span></p></div>
+
+		<p class="detail">Les CSS (Cascading Style Sheet) sont la façon la plus agile de définir l'apparence d'une page web. Je les utilise systématiquement, et les manie avec avec grand soin : compatibilité sur les navigateurs principaux du marché, emploi du CSS Orienté Objet pour des pages web belles et rapides !</p>
+
+	    </div>
+
+	    <div class="skill">
+
+		<h2>Futuriste</h2>
+
+		<div class="mark9"><p><span>9/10</span></p></div>
+
+		<p class="detail">
+
+		    J'ai un intérêt fort pour les technologies émergentes, telles que HTML5 et CSS3.
+
+		    Leur standardisation n'arrivera probablement qu'en 2022, mais les navigateurs actuels implémentent largement ces standard en puissance, le site web sur lequel vous êtes en est la preuve !
+
+		</p>
+
+	    </div>
+
+	    <div class="skill">
+
+		<h2>Adobe Master</h2>
+
+		<div class="mark6"><p><span>6/10</span></p></div>
+
+		<p class="detail">
+
+		    Je connais et j'utilise régulièrement des outils de la suite Adobe : Photoshop, Illustrator et Fireworks en tête.
+
+		    J'ai aussi touché à Soundbooth, InDesign, Premiere, AfterEffects. (brèves notions)
+
+		</p>
+
+	    </div>
+
+
+
+	    <div class="skill">
+
+		<h2>Comportementaliste</h2>
+
+		<div class="mark7"><p><span>7/10</span></p></div>
+
+		<p class="detail">Bonne de connaissance de Javascript et maîtrise de Jquery (Framework pour Javascript). Ce language est un standard incontesté en matière de dynamisme des pages web. J'essaie de l'implémenter de façon non-intrusive, pour permettre à ceux qui n'activent pas Javascript de naviguer sur le web sans gêne.</p>
+
+	    </div>
+
+	    <div class="skill">
+
+		<h2>Côté serveur...</h2>
+
+		<div class="mark8"><p><span>8/10</span></p></div>
+
+		<p class="detail">Maîtrise de PHP procédural et orienté objet : je suis capable de produire des applications complexes et robustes en PHP, notamment grâce à l'utilisation de framework : CodeIgniter ou Wordpress font partie de mes armes de choix.</p>
+
+	    </div>
+
+
+
+	</section>
+
+
+
+
+	<footer>
+
+	    <p>Copyright © 2010 Arnaud Brousseau | Powered by CodeIgniter | Site valide HTML5 | Disposition fluide (redimmensionnez votre navigateur) | Version mobile : à venir</p>
+
+	</footer>
+
+
+
+        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js" type="text/javascript"></script>
+
+	<script>!window.jQuery && document.write('<script src="/portfolio/js/jquery-1.4.2.min.js"><\/script>')</script>
+
+        <script src="/portfolio/js/jquery.scrollTo-1.4.2-min.js" type="text/javascript"></script>
+
+	<script src="/portfolio/js/gaming.js"></script>
+
+        <!--[if lt IE 7 ]>
+
+            <script src="js/dd_belatedpng.js?v=1"></script>
+
+        <![endif]-->
+
+
+
+      <script>
+
+       var _gaq = [['_setAccount', 'UA-XXXXX-X'], ['_trackPageview']];
+
+       (function(d, t) {
+
+        var g = d.createElement(t),
+
+            s = d.getElementsByTagName(t)[0];
+
+        g.async = true;
+
+        g.src = '//www.google-analytics.com/ga.js';
+
+        s.parentNode.insertBefore(g, s);
+
+       })(document, 'script');
+
+      </script>
+
+    </body>
+
+
+
+</html>
