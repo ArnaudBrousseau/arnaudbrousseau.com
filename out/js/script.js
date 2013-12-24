f6423982c98d273c2cae4b49a3678e9a7c33eb4f
Arnaud Brousseau <arnaud.brousseau@gmail.com>
2013-12-24 10:38:28 -0800

    Gets rid of the view source horrendous script. Favors a link to Github instead.

--- out/js/script.js
+++ out/js/script.js
@@ -1,82 +1,9 @@
-/*********************
+/*************************
   Author: Arnaud Brousseau
- *********************/
+ *************************/
 $(document).ready(function() {
     $('.toTop').click(function(){
         $('html, body').animate({scrollTop:0});
         return false;
     });
 });
-
-/* Credits to Remy Sharp for this piece of code ! */
-var addEvent = (function () {
-  if (document.addEventListener) {
-    return function (el, type, fn) {
-      if (el && el.nodeName || el === window) {
-        el.addEventListener(type, fn, false);
-      } else if (el && el.length) {
-        for (var i = 0; i < el.length; i++) {
-          addEvent(el[i], type, fn);
-        }
-      }
-    };
-  } else {
-    return function (el, type, fn) {
-      if (el && el.nodeName || el === window) {
-        el.attachEvent('on' + type, function () { return fn.call(el, window.event); });
-      } else if (el && el.length) {
-        for (var i = 0; i < el.length; i++) {
-          addEvent(el[i], type, fn);
-        }
-      }
-    };
-  }
-})();
-
-(function () {
-
-var pre = document.createElement('pre');
-pre.id = "view-source";
-
-// private scope to avoid conflicts
-addEvent(window, 'click', function (event) {
-  if (event.target.hash == '#view-source') {
-    // event.preventDefault();
-    if (!document.getElementById('view-source')) {
-      pre.innerHTML = ('<!DOCTYPE html>\n    <html>\n    ' + document.documentElement.innerHTML + '\n</html>').replace(/[<>]/g, function (m) { return {'<':'&lt;','>':'&gt;'}[m]});
-      document.body.appendChild(pre);
-    }
-    document.body.className = 'view-source';
-
-    var sourceTimer = setInterval(function () {
-      if (window.location.hash != '#view-source') {
-        clearInterval(sourceTimer);
-        document.body.className = '';
-      }
-    }, 200);
-  }
-});
-
-})();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
