d12e2e445d9006053829b31a8ab55a5b9a621287
Arnaud Brousseau <arnaud.brousseau@gmail.com>
2014-03-16 11:30:12 -0700

    Deletes out/ folder.

--- out/js/script.js
+++
@@ -1,9 +0,0 @@
-/*************************
-  Author: Arnaud Brousseau
- *************************/
-$(document).ready(function() {
-    $('.toTop').click(function(){
-        $('html, body').animate({scrollTop:0});
-        return false;
-    });
-});
